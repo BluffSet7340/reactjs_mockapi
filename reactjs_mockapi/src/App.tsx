3ea50ea0b932a7c8a6000835f7f234bd937515f7
Saeed Bin Mizan <sbm7640@outlook.com>
2025-02-24 12:38:49 +0400

    frontend tweaks and added the db.json file

--- reactjs_mockapi/src/App.tsx
+++ reactjs_mockapi/src/App.tsx
@@ -55,20 +55,21 @@ function App() {
   return (
     <div className="">
       {/* adding a new card */}
-      <div className="flex flex-row items-center justify-center p-4 bg-gray-200">
+      <div className="flex flex-col sm:flex-row items-center justify-center p-4 bg-gray-200
+      space-y-4 sm:space-y-0 sm:space-x-4">
         <input
           type="text"
           placeholder="Title"
           value={title}
           onChange={e => setTitle(e.target.value)}
-          className="p-2 mx-2 border border-gray-300 rounded-md"
+          className="p-2 mx-2 border border-gray-300 rounded-md w-64"
         />
         <input
           type="text"
           placeholder="Description"
           value={desc}
           onChange={e => setDesc(e.target.value)}
-          className="p-2 mx-2 border border-gray-300 rounded-md"
+          className="p-2 mx-2 border border-gray-300 rounded-md w-64"
         />
         <button onClick={addPost} className="p-2 mx-2 bg-blue-500 text-white rounded-md">Add Post</button>
       </div>
@@ -77,13 +78,13 @@ function App() {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-40 mt-10">
           {/* fetches from fake api and maps through them to display each post as a card */}
           {posts && posts.map(post => (
-            <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
+            <div className="block max-w-sm min-w-[300px] p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100">
               <div className="flex justify-between items-start">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{post.title}</h5>
+                <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">{post.title}</h5>
                 {/* clicking this button should delete the post from webpage and api*/}
                 <button className="delete ml-5" onClick={() => deletePost(post.id)}>X</button>
               </div>
-              <p className="font-normal text-gray-700">{post.desc}</p>
+              <p className="font-normal text-sm text-gray-700">{post.desc}</p>
             </div>
           ))}
         </div>
